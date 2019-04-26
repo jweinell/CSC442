65673ab6d526e6312c851562df4c1697e2a1f2fa
Christohper Rice <christopherrice9@gmail.com>
2019-04-25 21:47:14 -0500

    Updated code -- needs timezone fix still

--- timelock/timelock_wc.py
+++ timelock/timelock_wc.py
@@ -1,15 +1,17 @@
 import hashlib
 import datetime
 import pytz
+import dateutil
 
-#### NOTE: THIS CODE REQUIRES PYTZ LIBRARY. MUST BE INSTALLED BEFORE RUNNING. ####
+#### NOTE: THIS CODE REQUIRES PYTZ / DATEUTIL LIBRARY. MUST BE INSTALLED BEFORE RUNNING. ####
 #### pip install pytz ####
+#### pip install python-dateutil ####
 
 # datetimes should be stored as YYYY MM DD HH mm SS
 # temporary testing values
 # will later be taken in from stdin and etc.
-INPUT_EPOCH = "1999 12 31 23 59 59"
-CURRENT_TIME = "2017 04 23 18 02 30"
+INPUT_EPOCH = "2014 12 31 00 00 00"
+CURRENT_TIME = "2015 01 01 00 01 30"
 
 # turn the input into a valid datetime object
 epoch = INPUT_EPOCH.split()
@@ -27,7 +29,29 @@ print time_elapsed
 # and subtract those differences
 time_elapsed = time_elapsed - (time_elapsed % 60)
 print time_elapsed
-
+time_elapsed_round = long(time_elapsed)
+print time_elapsed_round
 # now based on THIS time difference, we perform the double MD5 hashing.
-hashed = hashlib.md5(hashlib.md5(str(time_elapsed)).hexdigest()).hexdigest()
+hashed = hashlib.md5(hashlib.md5(str(time_elapsed_round)).hexdigest()).hexdigest()
 print hashed
+
+# now we need to convert this into our specific key
+output = ""
+counter_af = 0
+counter_09 = 0
+# grav the first two characters
+for item in hashed:
+	if counter_af >= 2:
+		break
+	elif item.isalpha() == True:
+		output += item
+		counter_af += 1
+# grab the first two numbers from reverse order
+hashed_rev = hashed[::-1]
+for item in hashed_rev:
+	if counter_09 >= 2:
+		break
+	elif item.isdigit() == True:
+		output += item
+		counter_09 += 1
+print output
