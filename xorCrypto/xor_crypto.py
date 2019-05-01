7882feeeb6c3777396d19a4559580cfa36a091f2
Christohper Rice <christopherrice9@gmail.com>
2019-04-30 21:21:01 -0500

    XOR stuff

--- xorCrypto/xor_crypto.py
+++ xorCrypto/xor_crypto.py
@@ -31,54 +31,13 @@ def XOR():
 	if (DEBUG):
 		print "key and input text were the same size, valid"
 
-	# now that we know the key and token are the same size, we can perform operations on them
-	# but to be able to XOR them, they must be in their binary form first
-	key_bin = ""
-	input_bin = ""
-	# turn the key/input into a binary string that we can perform actions on
-	# REMINDER: THIS IS IN EIGHT-BIT ASCII
-	for item in range(0, len(key) - 1):
-		key_bin += str(bin(ord(key[item]))[2:]).zfill(8)
-	for item in range(0, len(input_text) - 1):
-		input_bin += str(bin(ord(input_text[item]))[2:]).zfill(8)
+	output_str = ""
+	i = 0
+	while i < len(input_text):
+		output_str += chr(ord(input_text[i]) ^ ord(key[i]))
+		i += 1
 
-	if (DEBUG):
-		print "key converted to binary: {}".format(key_bin)
-		print "input converted to binary: {}".format(input_bin)
-
-	# now we can generate our encoded output based on the key
-	output_str = str(bin(int(key_bin, 2) ^ int(input_bin, 2))[2:])
-	output_str = output_str[2:]
-
-	if (DEBUG):
-		print "output str pre-ascii-reconversion: {}".format(output_str)
-
-	# convert this back to ASCII
-	ascii_output = decode_ascii(output_str, 8)
-	sys.stdout.write(ascii_output)
-		
-
-# ASCII Decoder, from previous coding assignment
-def decode_ascii(binstr, bits):
-    modification = bits - len(binstr)%bits
-    index = 0
-    mod_list = []
-    while index < modification:
-        binstr = binstr + '0'
-        index = index + 1
-    chrarray = []
-    while len(binstr) > 0:
-        chrnum = int(binstr[:bits],2)
-        if chrnum != 8:
-            chrarray.append(chr(chrnum))
-        else:
-            if len(chrarray) > 0:
-                chrarray = chrarray[:-1]
-        binstr = binstr[bits:]
-    decoded = "".join(chrarray)
-    return decoded
-
-
+	sys.stdout.write(output_str)
 
 ## MAIN CODE ##
 XOR()
